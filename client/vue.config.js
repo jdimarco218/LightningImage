29b158b8e23653d0d522608ca8f29dabf487339e
Jeff DiMarco <jdimarco218@gmail.com>
2019-02-27 19:41:17 -0500

    Server side ports to env PORT

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
+const port = process.env.PORT || 8082;
 
 module.exports = {
     outputDir: path.resolve(__dirname, '../server/public'),
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://localhost:5008'
+                target: `http://localhost:${port}`
             }
         }
     }
--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 var http = require('http');
-const port = process.env.PORT || 5008;
+const port = process.env.PORT || 8082;
 
 // Middleware
 app.use(bodyParser.json());
