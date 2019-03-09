d8e57f31f3c3beacb636e6f41047ab94724dca34
jdimarco218 <jdimarco218@gmail.com>
2019-03-08 23:33:58 -0500

    Updated title and favicon

--- client/vue.config.js
+++ client/vue.config.js
@@ -3,6 +3,11 @@ const port = process.env.VUE_APP_SERVER_PORT || 8082;
 const host = process.env.VUE_APP_SERVER_HOST || "69.141.47.76";
 
 module.exports = {
+    pwa: {
+        iconPaths: {
+            favicon32: 'src/assets/favicon.png'
+        }
+    },
     outputDir: path.resolve(__dirname, '../server/public'),
     devServer: {
         proxy: {
